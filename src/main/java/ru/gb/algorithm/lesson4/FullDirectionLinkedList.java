afd94fb15022dc22645d0266468d95a0eb071ecb
petrovks <38166811+petrovks@users.noreply.github.com>
2021-10-28 11:08:14 +0300

    Homework3

---
+++ src/main/java/ru/gb/algorithm/lesson4/FullDirectionLinkedList.java
@@ -0,0 +1,6 @@
+package ru.gb.algorithm.lesson4;
+
+public interface FullDirectionLinkedList<E> extends TwoSideLinkedList<E> {
+
+    E removeLast();
+}
---
+++ src/main/java/ru/gb/algorithm/lesson4/TwoSideLinkedListImpl.java
@@ -0,0 +1,84 @@
+package ru.gb.algorithm.lesson4;
+
+public class TwoSideLinkedListImpl<E> extends SimpleLinkedListImpl<E> implements TwoSideLinkedList<E> {
+
+    protected Node<E> last;
+
+    @Override
+    public void insertFirst(E value) {
+        super.insertFirst(value);
+        if (size == 1) {
+            last = first;
+        }
+    }
+
+    protected void insertLastNode(Node<E> newNode) {
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+        size++;
+    }
+
+    @Override
+    public void insertLast(E value) {
+        Node<E> newNode = new Node<>(value, null);
+
+        if (isEmpty()) {
+            insertFirst(value);
+            return;
+        }
+
+        last.next = newNode;
+        last = newNode;
+        size++;
+    }
+
+    @Override
+    public E removeFirst() {
+        E removedValue = super.removeFirst();
+
+        if (isEmpty()) {
+            last = null;
+        }
+
+        return removedValue;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        Node<E> current = first;
+        Node<E> prev = null;
+
+        while (current != null) {
+            if (current.item.equals(value)) {
+                break;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            return false;
+        } else if (current == first) {
+            removeFirst();
+            return true;
+        } else if(current == last) {
+            last = prev;
+            last.next = null;
+        }
+        prev.next = current.next;
+
+        current.next = null;
+        size--;
+
+        return true;
+    }
+
+    @Override
+    public E getLast() {
+        return last.item;
+    }
+}
